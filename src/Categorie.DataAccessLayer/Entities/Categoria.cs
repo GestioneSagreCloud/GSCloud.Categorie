a8b0b9c4fb2f69adb413720bc460947a6cba066f
Angelo Pirola <angelo9871@gmail.com>
2026-06-29 12:54:56 +0200

    Add Categoria entity with video and print category fields
    Introduced the Categoria class in the Categorie.DataAccessLayer.Entities namespace. This entity inherits from BaseEntity<int> and includes IdFesta, Categoria_Video, and Categoria_Stampa properties.

---
+++ src/Categorie.DataAccessLayer/Entities/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace Categorie.DataAccessLayer.Entities;
+
+public class Categoria : BaseEntity<int>
+{
+    public int IdFesta { get; set; }
+    public string Categoria_Video { get; set; } = null!;
+    public string Categoria_Stampa { get; set; } = null!;
+}
