7d612effa5f061ce7f9d036ca0902e7a8aa6ec0f
Angelo Pirola <angelo9871@gmail.com>
2026-06-29 12:54:40 +0200

    Add BusinessLayer project with custom failures and global usings
    Added Categorie.BusinessLayer.csproj targeting .NET 10.0 with implicit usings and nullable enabled. Introduced CustomFailureReasons for error codes and GlobalUsings for shared namespaces. Added reference to DataAccessLayer project.

---
+++ src/Categorie.BusinessLayer/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using AppEngine.Routing;
+global using AppEngine.Tools.OperationResults;
+global using AppEngine.Tools.TimeZoneService.Interfaces;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.Extensions.Logging;
