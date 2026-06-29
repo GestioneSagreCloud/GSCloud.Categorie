8ad35e33f405004ae79eeacad1ca52d3e4dafa2f
Angelo Pirola <angelo9871@gmail.com>
2026-06-29 12:55:14 +0200

    Add ApplicationDbContext with Categoria entity mapping
    Added ApplicationDbContext class with DbSet for Categoria and entity configuration. Updated project file with EF Core and TinyAppEngine dependencies. Introduced global usings for EF Core entities and namespaces.

---
+++ src/Categorie.DataAccessLayer/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Categorie.DataAccessLayer.Entities;
+
+namespace Categorie.DataAccessLayer;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public virtual DbSet<Categoria> Categorie { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.ToTable("Categorie");
+            entity.HasKey(e => e.Id).HasName("PK_Categorie");
+        });
+    }
+}
---
+++ src/Categorie.DataAccessLayer/GlobalUsings.cs
@@ -0,0 +1,2 @@
+﻿global using AppEngine.EFCore.Entities;
+global using Microsoft.EntityFrameworkCore;
