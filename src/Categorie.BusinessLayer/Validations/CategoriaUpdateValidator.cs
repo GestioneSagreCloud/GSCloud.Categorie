aeda593675b0ad246bd95c7b2b6648571d52bab5
Angelo Pirola <angelo9871@gmail.com>
2026-06-29 12:54:30 +0200

    Add validators for Categoria create and update models
    Introduced CategoriaCreateValidator and CategoriaUpdateValidator using FluentValidation. These enforce that IdFesta is greater than 0, and both Categoria_Video and Categoria_Stampa are required and limited to 100 characters, with specific error messages.

---
+++ src/Categorie.BusinessLayer/Validations/CategoriaUpdateValidator.cs
@@ -0,0 +1,21 @@
+﻿using Categorie.BusinessLayer.Models;
+using FluentValidation;
+
+namespace Categorie.BusinessLayer.Validations;
+
+public class CategoriaUpdateValidator : AbstractValidator<CategoriaUpdateModel>
+{
+    public CategoriaUpdateValidator()
+    {
+        RuleFor(x => x.IdFesta)
+            .GreaterThan(0).WithMessage("IdFesta must be greater than 0.");
+
+        RuleFor(x => x.Categoria_Video)
+            .NotEmpty().WithMessage("La categoria video è obbligatoria.")
+            .MaximumLength(100).WithMessage("La categoria video non può superare i 100 caratteri.");
+
+        RuleFor(x => x.Categoria_Stampa)
+            .NotEmpty().WithMessage("La categoria stampa è obbligatoria.")
+            .MaximumLength(100).WithMessage("La categoria stampa non può superare i 100 caratteri.");
+    }
+}
